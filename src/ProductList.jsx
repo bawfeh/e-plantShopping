65b8bab9eb0d87005a92c2f86fda8dff8b598e0b
bawfeh <bawfeh@gmail.com>
2025-08-28 08:31:55 -0400

    completed final project

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,7 +7,18 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
-    const plantsArray = [
+    /*const [plantsArray, setPlantsArray] = useState([]);
+    // download data using side effect
+    useEffect(() => {
+        fetch("./products.json")
+          .then((response) => response.json())
+          .then((data) => setPlantsArray(data))
+          .catch((e) => {
+            console.error("Error downloading products data");
+            console.error(`Error thrown: ${e}`);
+          });
+      }, []);*/
+      const plantsArray = [
         {
             category: "Air Purifying Plants",
             plants: [
@@ -214,6 +225,7 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
+  
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -233,12 +245,6 @@ function ProductList({ onHomeClick }) {
         color: 'white',
         fontSize: '30px',
         textDecoration: 'none',
-    }
-    const styleB = {
-        display: 'flex',
-        alignIems: 'center',
-        justifyContent: 'space-between',
-        gap: '-10px',
     }
     const dispatch = useDispatch();
     const handleAddToCart = (product) => {
@@ -293,17 +299,14 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div style={styleB}>
-                        <div>{calculateTotalQuantity()}</div>
-                        <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
-                    </div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><div className='cart_quantity_count'>{calculateTotalQuantity()}</div><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
             <div className="product-grid">
                 {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
                 <div key={index}> {/* Unique key for each category div */}
-                    <h1> <div>{category.category}</div> </h1>{/* Display the category name */}
+                    <div className="product-category"><h1> {category.category} </h1></div>{/* Display the category name */}
                     <div className="product-list"> {/* Container for the list of plant cards */}
                         {category.plants.map((plant, plantIndex) => ( // Loop through each plant in the current category
                         <div className="product-card" key={plantIndex}> {/* Unique key for each plant card */}
